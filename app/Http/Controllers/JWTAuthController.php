12d18d0a7d16d7fc3340b1a0a40a0f2371ddefdf
Lucas Antunes <lucasantuneslara06@gmail.com>
2024-11-05 09:00:15 -0300

    Aula 05/11 - Cadastro, login e token JWT

---
+++ app/Http/Controllers/JWTAuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class JWTAuthController extends Controller {
+    public function register(Request $request) {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user', 'token'), 201);
+    }
+
+    public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (!JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+
+            $user = auth()->user();
+
+            # No caso de atribuir um papel
+            #$token = JWTAuth::claims(['role' => $user->role])->fromUser($user);
+            $token = JWTAuth::fromUser($user);
+
+            return response()->json(compact('token'));
+        } catch (JWTException $exception) {
+            return response()->json(['error' => 'Could not create token'], 500);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JWTAuthController;
 use App\Http\Controllers\PeopleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,23 +11,14 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/somar', function (Request $request) {
-    if (count($request->all()) < 1) {
-        return response()->json([
-            'message' => 'Não há valores para somar'
-        ], 406);
-    }
-    $soma = array_sum($request->all());
-    return response()->json([
-        'message' => 'Somado com sucesso', // Opcional
-        'sum' => $soma,
-    ]);
-});
-
 Route::prefix('people')->group(function () {
     Route::get('list', [PeopleController::class, 'list']);
 
     Route::post('store', [PeopleController::class, 'store']);
 
     Route::post('storeInterest', [PeopleController::class, 'storeInterest']);
+});
+
+Route::prefix('user')->group(function () {
+    Route::post('register', [JWTAuthController::class, 'register']);
 });
